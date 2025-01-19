4a488ff73a8cba48b4e156faf979375dec5bb21a
Ialimeva <mevashy@gmail.com>
2025-01-19 12:21:07 +0300

    add record path

--- Todolist/Todolist/urls.py
+++ Todolist/Todolist/urls.py
@@ -20,7 +20,6 @@ from home.views import home_view, add_view, addrecord_view
 
 urlpatterns = [
     path('', home_view),
-    path('add/', add_view),
-    path('home/', home_view),
+    path('add/addrecord', add_view),
     path('admin/', admin.site.urls),
 ]
