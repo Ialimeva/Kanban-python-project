1ffc2295058d92578f5a37ef7d330c6f55c53765
Ialimeva <mevashy@gmail.com>
2025-01-19 14:06:11 +0300

    change date format
    change date format do a format django can read and error handling

--- Todolist/home/views.py
+++ Todolist/home/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from home.models import Tasklist
 from django.utils.timezone import now
+from django.utils.dateparse import parse_datetime  # This imports Django's tool for understanding dates
 
 def home_view(request):
     tasklist = Tasklist.objects.all().values()
@@ -17,10 +18,20 @@ def addrecord_view(request):
         start_date = request.POST.get ('start_date')
         finish_date = request.POST.get ('finish_date', default=now)
         if title and start_date and finish_date:
-            Tasklist.objects.create(
-                title = title,
-                start_date = start_date,
-                finish_date = finish_date
-            )
+            try:  # This is like saying "try to do this, but if it fails, don't crash"
+            # Convert the dates to a format Django understands
+            # The form sends dates like "2024-01-19T10:00"
+            # We replace 'T' with space to make "2024-01-19 10:00"
+                start_datetime = parse_datetime(start_date.replace('T', ' '))
+                finish_datetime = parse_datetime(finish_date.replace('T', ' '))
+            
+            # Create new task in database with the converted dates
+                Tasklist.objects.create(
+                title=title,
+                start_date=start_datetime,
+                finish_date=finish_datetime
+                )
+            except Exception as e:  # If anything goes wrong...
+                print(f"Error: {e}")  # ...print the error (helps with debugging)
         return redirect('home')
     return redirect('add')
